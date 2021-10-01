067efd6a189873b59d4aafb0d1a6cacdd3cf0e65
유지원 <aprilwldnjs@gmail.com>
2021-10-01 16:01:22 +0900

    fix nav

--- src/routers/navsRouter.js
+++ src/routers/navsRouter.js
@@ -2,18 +2,17 @@ import express from "express";
 
 const navsRouter = express.Router();
 
-navsRouter.get('/', (req, res) => {
-  res.render('index');
-})
-navsRouter.get('/howtocome', (req, res) => {
-  res.send('howtocome');
-})
-navsRouter.get('/tips', (req, res) => {
-  res.send('tips');
-})
-navsRouter.get('/comments', (req, res) => {
-  res.send('comments');
-})
-
+navsRouter.get("/", (req, res) => {
+  res.render("index");
+});
+navsRouter.get("/howtocome", (req, res) => {
+  res.render("howtocome");
+});
+navsRouter.get("/tips", (req, res) => {
+  res.render("tips");
+});
+navsRouter.get("/comments", (req, res) => {
+  res.render("comments");
+});
 
 export default navsRouter;
