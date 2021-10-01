24c1cc1990bbbaacfd537989a466db98fc0ab905
Yonghyun <yh47529722@gmail.com>
2021-10-01 14:05:18 +0900

    make navsRouters and add ejs

--- src/routers/navsRouter.js
+++ src/routers/navsRouter.js
@@ -5,5 +5,15 @@ const navsRouter = express.Router();
 navsRouter.get('/', (req, res) => {
   res.render('index');
 })
+navsRouter.get('/howtocome', (req, res) => {
+  res.send('howtocome');
+})
+navsRouter.get('/tips', (req, res) => {
+  res.send('tips');
+})
+navsRouter.get('/comments', (req, res) => {
+  res.send('comments');
+})
+
 
 export default navsRouter;
